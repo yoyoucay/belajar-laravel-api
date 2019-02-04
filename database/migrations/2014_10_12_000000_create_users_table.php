dbd09e6ad122359d0c93aee7d90fd71e46474fac
Beemons <beemons123@gmail.com>
2019-02-04 14:53:22 +0700

    Setting Up API Project

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username')->unique();
+            $table->string('username', 100)->unique();
             $table->string('email', 100)->unique();
             $table->string('password');
             $table->timestamps();
